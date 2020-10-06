8c863970a550612dfe2b98055ea112995907f175
PJ Waskiewicz <pjwaskiewicz@gmail.com>
2020-10-06 01:53:26 -0700

    etcd-bootstrap: finish etcd loader with initial json-based schema
    Load the etcd3 backend target with an initial sample dataset, seeded
    by a JSON representation of the data, with hierarchy.
    
    Signed-off-by: PJ Waskiewicz <pjwaskiewicz@gmail.com>

--- etcd-loader.py
+++ etcd-loader.py
@@ -10,10 +10,44 @@ import etcd3
 host = "10.10.1.2"
 port = 2379
 
+# open the etcd target
 client = etcd3.client(host=host, port=port)
 
-key = client.get('/foo/baz/booze')
+# read in the json schema
+with open("./input.json", "r") as f:
+    data = json.load(f)
 
-print(key)
+# Load the etcd3 database
+for host in data['hosts']:
+    hostname = host.get('hostname')
+    print ('/hosts/' + hostname + '/hostname \t\t\t ===> ', hostname)
+    print ('/hosts/' + hostname + '/available \t\t\t ===> ', host.get('available'))
+    client.put('/hosts/' + hostname + '/hostname', hostname)
+    client.put('/hosts/' + hostname + '/available', host.get('available'))
 
+    # network config block
+    for network in host['network']:
+        print ('/hosts/' + hostname + '/network/ipaddr \t\t ===> ', network.get('ipaddr'))
+        print ('/hosts/' + hostname + '/network/gateway \t\t ===> ', network.get('gateway'))
+        print ('/hosts/' + hostname + '/network/netmask \t\t ===> ', network.get('netmask'))
+        client.put('/hosts/' + hostname + '/network/ipaddr', network.get('ipaddr'))
+        client.put('/hosts/' + hostname + '/network/gateway', network.get('gateway'))
+        client.put('/hosts/' + hostname + '/network/netmask', network.get('netmask'))
+
+        for ethernet in network['ethernet']:
+            print ('/hosts/' + hostname + '/network/ethernet/mtu \t ===> ', ethernet.get('mtu'))
+            print ('/hosts/' + hostname + '/network/ethernet/fc-mode \t ===> ', ethernet.get('fc-mode'))
+            client.put('/hosts/' + hostname + '/network/ethernet/mtu', ethernet.get('mtu'))
+            client.put('/hosts/' + hostname + '/network/ethernet/fc-mode', ethernet.get('fc-mode'))
+
+    # blinkstick config data block
+    for blinkstick in host['blinkstick']:
+        print ('/hosts/' + hostname + '/blinkstick/frontColor \t ===> ', blinkstick.get('frontColor'))
+        print ('/hosts/' + hostname + '/blinkstick/backColor \t ===> ', blinkstick.get('backColor'))
+        print ('/hosts/' + hostname + '/blinkstick/blinkRate \t ===> ', blinkstick.get('blinkRate'))
+        client.put('/hosts/' + hostname + '/blinkstick/frontColor', blinkstick.get('frontColor'))
+        client.put('/hosts/' + hostname + '/blinkstick/backColor', blinkstick.get('backColor'))
+        client.put('/hosts/' + hostname + '/blinkstick/blinkRate', blinkstick.get('blinkRate'))
+
+# shutdown etcd3 connection
 client.close()
